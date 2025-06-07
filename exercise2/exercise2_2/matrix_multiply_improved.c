543f2c9ea6615698fc84704ca6d0e9c626fd89f2
chenfeng1212 <144661725+chenfeng1212@users.noreply.github.com>
2025-06-07 20:03:04 +0000

    Add files via upload

--- exercise2/exercise2_2/matrix_multiply_improved.c
+++ exercise2/exercise2_2/matrix_multiply_improved.c
@@ -1,4 +1,12 @@
 void matrix_multiply(int *a, int *b, int *output, int i,
                            int k, int j) {
     // Inplement your code here
+    for (int x = 0; x < i; x++) {
+        for (int z = 0; z < k; z++) {
+            int a_val = a[x * k + z];
+            for (int y = 0; y < j; y++) {
+                output[x * j + y] += a_val * b[z * j + y]; 
+            }
+        }
+    }
 }
