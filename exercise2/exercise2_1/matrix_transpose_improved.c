455a8d1e892fada04a7e5290f30ddf9e8815f2e8
chenfeng1212 <144661725+chenfeng1212@users.noreply.github.com>
2025-06-07 20:01:57 +0000

    Add files via upload

--- exercise2/exercise2_1/matrix_transpose_improved.c
+++ exercise2/exercise2_1/matrix_transpose_improved.c
@@ -1,3 +1,14 @@
 void matrix_transpose(int n, int *dst, int *src) {
     // Inplement your code here
+    int B = 8;
+
+    for (int i = 0; i < n; i += B) {
+        for (int j = 0; j < n; j += B) {
+            for (int x = i; x < i + B && x < n; ++x) {
+                for (int y = j; y < j + B && y < n; ++y) {
+                    dst[y * n + x] = src[x * n + y]; 
+                }
+            }
+        }
     }
+}
